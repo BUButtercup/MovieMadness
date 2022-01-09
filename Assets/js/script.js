f0b3957726d6ecdeb2fa54767c3b15819c2580ac
Henry Weigand <henryweigand10@gmail.com>
2022-01-08 21:01:07 -0800

    started w foundation, started a folder that contains an entire project using foundation but haven't deleted the materializes files yet

---
+++ Assets/js/script.js
@@ -0,0 +1,17 @@
+// dropdown
+// $('.dropdown-trigger').dropdown();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var options = {
+        dropdownOptions: {
+            alignment: 'bottom',
+            hover: false,
+            coverTrigger: false,
+            closeOnClick: true
+        }
+    }
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+
