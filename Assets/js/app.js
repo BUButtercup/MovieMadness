8aee3eaa490d39e8ba29658b67804004730d98a4
Buttercup <andyandnile@gmail.com>
2022-01-12 12:57:35 -0800

    1. in app.js cut down search results to an array of 5, stored in localStorage, 2. added onload listener to results.js, which pulls info from local and runs cardMaker function

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -3,7 +3,8 @@ $(document).foundation()
 // dropdown
 // $('.dropdown-trigger').dropdown();
 
-let fetchResult = [];
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
@@ -16,31 +17,26 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
     //var instances = M.Dropdown.init(elems, options);
-  });
+});
 
-  function makeListCard(data)
-    let genreArr = data.genre_ids;
-    let genreCont = [];
-    for (let i=0; i<genreArr.length; i++){
-        let genreName = genreArr[i].name;
-        genreCont.push(genreName);
-    }
-    genreCont = genreCont.join(' / ');
-    console.log(genreCont);
+
+
 
 // Fetch TMDB API for genre
 var getMovieByGenre = function(genre){
         fetch('https://api.themoviedb.org/3/discover/movie?api_key=734711869501c48d5ea1cb162098c006&with_genres='+genre)
         .then(response => response.json())
     .then(data => {
-        fetchResult = data
-        console.log(fetchResult)
-        makeListCard(fetchResult)
+        let movieObjects = Array.from(data.results)
+        console.log(movieObjects)
+        movieObjects = movieObjects.slice(0, 5); //cutting it down to an array of 5 movie objects
+        console.log(movieObjects);
+        localStorage.setItem('movieObjects', JSON.stringify(movieObjects));
     });
 };
 // Fetch TMDB API for year
-var getMovieByYear = function(year){
-        fetch('https://api.themoviedb.org/3/discover/movie?api_key=734711869501c48d5ea1cb162098c006&primary_release_year='+year)
-        .then(response => response.json())
-    .then(data => console.log(data));
-    };
+// var getMovieByYear = function(year){
+//         fetch('https://api.themoviedb.org/3/discover/movie?api_key=734711869501c48d5ea1cb162098c006&primary_release_year='+year)
+//         .then(response => response.json())
+//     .then(data => console.log(data));
+//     };
---
+++ Assets/js/results.js
@@ -0,0 +1,59 @@
+
+
+function makeListCard(arrInd){ //what is feeding into here is the object that is stored within the array index location
+    let movieObj = arrInd       //the object fed into the fxn
+    let genreArr;               //holder for the genre names that have been assigned to the movie
+    let releaseYear;
+
+    console.log("movieObj : ", movieObj.title, movieObj);
+
+    function getGenres(){
+        genreArr = movieObj.genre_ids; 
+    
+    // for (let i=0; i < movieArr.length; i++){            
+    //   //get the genre id
+    //     let genreCont = [];
+    //     for (let i=0; i<genreArr.length; i++){
+    //         let genreName = genreArr[i].name;
+    //         genreCont.push(genreName);
+    //     }
+        genreArr = genreArr.join(' / ');            //put it in the genreCont as a joined string
+        console.log('genreArr: ', genreArr);
+    }
+
+  
+
+    function getReleaseYear() {                     //to get release year
+        let releaseDate = movieObj.release_date;
+        releaseYear = releaseDate.split('-');
+        releaseYear = parseInt(releaseYear[0]); //parse out release year as a number
+        console.log('releaseYear: ', releaseYear);
+    }
+        getGenres();
+        getReleaseYear();
+            
+        //vars to create elements
+        let createH4 = document.createElement('h4');
+        let createH5 = document.createElement('h5');
+        let infoP = document.createElement('p');
+        let movieBox = document.createElement('div');
+        let listCont = document.querySelector('#list-container');
+        
+        createH4.textContent = movieObj.title;
+        createH5.textContent = 'released ' + releaseYear + ' | ' + genreArr + '';
+        infoP.textContent = movieObj.overview;
+        movieBox.setAttribute('class', 'movieBox');
+        
+        movieBox.appendChild(createH4);
+        movieBox.appendChild(createH5);
+        movieBox.appendChild(infoP);
+        listCont.appendChild(movieBox);
+        
+};
+
+window.onload = (event) =>{
+    let movieObjects = JSON.parse(localStorage.getItem('movieObjects'));
+    for (let i=0; i< movieObjects.length; i++){      //for each of the first 5 movies on the list
+    makeListCard(movieObjects[i])
+    }
+};
